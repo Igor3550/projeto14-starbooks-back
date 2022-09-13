399688fb1a3626d9a7e30201b43a56eefd414e1e
Igor3550 <igormeloigormelo@gmail.com>
2022-09-13 18:15:22 -0300

    rota de teste

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ import cors from 'cors'
 const app = express()
 app.use(cors())
 
+const PORT = (process.env.PORT || 5000)
+
 app.get('/', (req, res) => {
   res.send('Acho que deu certo hein!')
 })
 
-app.listen(5000, () => console.log('Listen on port 5000'))
+app.listen(PORT, () => console.log(`Listen on port ${PORT}`));
