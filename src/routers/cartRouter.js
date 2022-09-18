d363ec66546f7356e46e5a398d8bec63f7f79ccd
Igor3550 <igormeloigormelo@gmail.com>
2022-09-17 21:52:26 -0300

    feat/cart delete item from cart

--- src/routers/cartRouter.js
+++ src/routers/cartRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import tokenVerify from '../middlewares/tokenMiddleware.js';
 import addOnCartMiddleware from '../middlewares/cartMiddleware.js';
-import { addOnCart, getCartItems } from '../controllers/cartController.js';
+import { addOnCart, deleteItem, getCartItems } from '../controllers/cartController.js';
 
 const router = express.Router();
 
 router.post('/cart/add/:bookId', tokenVerify, addOnCartMiddleware, addOnCart )
+router.delete('/cart/delete/:bookId', tokenVerify, addOnCartMiddleware, deleteItem )
 router.get('/cart', tokenVerify, getCartItems )
 
 export default router;
