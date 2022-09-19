68d7e5d7551218b548aa09d4ea0b7415ed9df036
ana-aranha <anab.aranha@gmail.com>
2022-09-19 07:32:54 -0300

    adding favorite route

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import authRouter from "./routers/authRouter.js";
 import booksRouter from "./routers/booksRouter.js";
 import cartRouter from "./routers/cartRouter.js";
 import purchaseRouter from "./routers/purchaseRouter.js";
+import favoritesRouter from "./routers/favoritesRouter.js";
 
 const app = express();
 app.use(express.json());
@@ -15,5 +16,6 @@ app.use(authRouter);
 app.use(booksRouter);
 app.use(cartRouter);
 app.use(purchaseRouter);
+app.use(favoritesRouter);
 
 app.listen(PORT, () => console.log(`Listen on port ${PORT}`));
