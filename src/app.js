5ac1c31a3660fa73bc3ad1e883ad6527ba4de200
ana-aranha <anab.aranha@gmail.com>
2022-09-19 05:06:23 -0300

    Adding purchase confirmation route

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import authRouter from "./routers/authRouter.js";
 import booksRouter from "./routers/booksRouter.js";
-import cartRouter from './routers/cartRouter.js';
+import cartRouter from "./routers/cartRouter.js";
+import purchaseRouter from "./routers/purchaseRouter.js";
 
 const app = express();
 app.use(express.json());
@@ -13,5 +14,6 @@ const PORT = process.env.PORT || 5000;
 app.use(authRouter);
 app.use(booksRouter);
 app.use(cartRouter);
+app.use(purchaseRouter);
 
 app.listen(PORT, () => console.log(`Listen on port ${PORT}`));
